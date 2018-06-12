794aa5f37899b48e6faa37bce9510b0a6781788e
jordan2802 <sha08dow@hotmail.fr>
2018-06-12 17:30:32 +0200

    boucle

---
+++ js/boucle.js
@@ -0,0 +1,18 @@
+//exos 1
+/*
+var i = 0;
+while (i<=10) {
+  console.log(i);
+  i++;
+}
+*/
+
+//exos 2
+var i = 0;
+var j = Math.floor(Math.random()*101) ;
+
+while (i<=20) {
+  var k = i*j;
+  console.log(k);
+  i++;
+}
