08d97901c1ae2162434fb341533e9c6d0c247446
jordan2802 <sha08dow@hotmail.fr>
2018-06-13 10:23:45 +0200

    exos boucle

--- js/boucle.js
+++ js/boucle.js
@@ -8,11 +8,74 @@ while (i<=10) {
 */
 
 //exos 2
+/*
 var i = 0;
-var j = Math.floor(Math.random()*101) ;
+var j = 50;
 
 while (i<=20) {
   var k = i*j;
   console.log(k);
   i++;
+}*/
+
+//exos3
+/*
+var i = 100;
+var j = 50;
+
+while (i>10) {
+  var k = i/j;
+  console.log(k);
+  i--;
+}*/
+
+//exos 4
+/*
+var i = 1;
+
+while (i<=10) {
+  console.log(i);
+  i+= i/2;
 }
+*/
+
+//exos5
+/*
+var i = 1;
+
+while (i<=15) {
+ var j = "on y arrive presque";
+  console.log(j);
+  i++;
+}*/
+
+//exos 6
+/*
+var i = 20;
+
+while (i>=0) {
+  var j = "C'est presque bon.";
+  console.log(j);
+  i--;
+}
+*/
+
+//exos 7
+/*
+var i = 1;
+
+while (i<=100) {
+ var j = "On tient le bon bout";
+  console.log(j);
+  i+=15;
+}*/
+
+//exos 8
+/*
+var i = 200;
+
+while (i>=0) {
+  var j = "Enfin";
+  console.log(j);
+  i-=12;
+}*/
